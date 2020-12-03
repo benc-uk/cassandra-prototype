a1401746769eeedbb71b6229a38eb754fdaff190
Ben Coleman <benc.uk@gmail.com>
2020-12-03 09:10:10 +0000

    blah

---
+++ run-cass-local.sh
@@ -0,0 +1,10 @@
+docker run \
+   --name local-cassandra-instance \
+   -p 7000:7000 \
+   -p 7001:7001 \
+   -p 7199:7199 \
+   -p 9042:9042 \
+   -p 9160:9160 \
+   -p 9404:9404 \
+   -d \
+   launcher.gcr.io/google/cassandra3

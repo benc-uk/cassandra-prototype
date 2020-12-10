5993cdbb829511b2e8189ce1c0fb1efcdea92d5c
Ben Coleman <benc.uk@gmail.com>
2020-12-10 17:48:45 +0000

    moving stuff

--- scripts/cassandra-k8s/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-helm repo add bitnami https://charts.bitnami.com/bitnami
-
-helm install cassdb bitnami/cassandra -f $DIR/values.yaml
